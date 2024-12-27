526975466553188b9948b4d94c5bfe33a1736427
Michael <makoy.ramirez@gmail.com>
2024-12-27 22:47:47 +0800

    refactored ContentView and OnboardingView

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -61,10 +61,43 @@ struct OnboardingView: View {
                         .fill(Color.white.opacity(0.2))
                         .padding(8)
                     // 2. CALL-TO-ACTION TEXT (STATIC)
+                    Text("Get Started")
+                        .font(.system(.title3,
+                                      design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
                     // 3. CAPSULE (DYNAMIC WIDTH)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
                     // 4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24,
+                                              weight: .bold))
+                        }//: ZSTACK
+                        .foregroundColor(.white)
+                        .frame(width: 80,
+                               height: 80,
+                               alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        Spacer()
+                    }//: HSTACK
                 }//: - FOOTER
-                .frame(height: 8, alignment: .center)
+                .frame(height: 80,
+                       alignment: .center)
                 .padding()
             } //: VSTACK
             .padding()
